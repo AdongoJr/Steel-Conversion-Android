6ad5736ea10cf0b4d17c9a4d65acc7e11751294d
AdongoJr <mosezadongo37@gmail.com>
2019-10-27 20:53:16 +0300

    initial release to playstore

--- app/src/main/java/com/app/steelconversion/MainActivity.kt
+++ app/src/main/java/com/app/steelconversion/MainActivity.kt
@@ -1,29 +1,19 @@
 package com.app.steelconversion
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
-import android.util.Log
 import android.view.MenuItem
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import com.mopub.mobileads.MoPubErrorCode
-import com.mopub.mobileads.MoPubView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var moPubBanner: MoPubView
-    private lateinit var moPubViewbannerAdListener: MoPubView.BannerAdListener
 
     private lateinit var dl: DrawerLayout
     private lateinit var t: ActionBarDrawerToggle
@@ -31,22 +21,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        /* MoPub Banner test Ad */
-        MyMoPub().init(this, getString(R.string.mBannerId))
-
-        Handler().postDelayed({
-            initBannerAds()
-        }, 200)
-
-
-        /* Going to SecondActivity */
-        val btn = findViewById<Button>(R.id.button)
-        btn.setOnClickListener {
-            val mIntent = Intent(this@MainActivity, SecondActivity::class.java)
-            startActivity(mIntent)
-        }
-
 
         val diameter = findViewById<EditText>(R.id.diameter_entry)
         val length = findViewById<EditText>(R.id.length_entry)
@@ -123,28 +97,10 @@ class MainActivity : AppCompatActivity() {
         nv.setNavigationItemSelectedListener { menuItem ->
 
             when(menuItem.itemId) {
-                R.id.settings -> {
-                    val mIntent = Intent(this@MainActivity, SettingsActivity::class.java)
-                    startActivity(mIntent)
-                }
                 R.id.exit -> {
                     finishAffinity()
                 }
-                R.id.help -> {
-                    val mIntent = Intent(this@MainActivity, HelpActivity::class.java)
-                    startActivity(mIntent)
-                }
-                R.id.change_theme -> {
-                    val mIntent = Intent(this@MainActivity, ThemesActivity::class.java)
-                    startActivity(mIntent)
-                }
-                R.id.share -> {
-                    Toast.makeText(this, "Share", Toast.LENGTH_LONG).show()
-                }
-                R.id.about -> {
-                    val mIntent = Intent(this@MainActivity, AboutActivity::class.java)
-                    startActivity(mIntent)
-                }
+
             }
 
             menuItem.isChecked = true
@@ -160,45 +116,6 @@ class MainActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
-
-    private fun initBannerAds() {
-        moPubBanner = findViewById(R.id.banner_view)
-        moPubBanner.adUnitId = getString(R.string.mBannerId)
-        moPubBanner.loadAd()
-
-        Log.d("MoPub", "Banner ad is Loading")
-
-        moPubViewbannerAdListener = object : MoPubView.BannerAdListener {
-            override fun onBannerExpanded(banner: MoPubView?) {
-                Log.d("MoPub", "Banner ad has expanded")
-            }
-
-            override fun onBannerLoaded(banner: MoPubView?) {
-                Log.d("MoPub", "Banner ad has loaded")
-            }
-
-            override fun onBannerCollapsed(banner: MoPubView?) {
-                Log.d("MoPub", "Banner ad has collapsed")
-            }
-
-            override fun onBannerClicked(banner: MoPubView?) {
-                Log.d("MoPub", "Banner ad was clicked")
-            }
-
-            override fun onBannerFailed(banner: MoPubView?, errorCode: MoPubErrorCode?) {
-                Log.d("MoPub", "Banner ad has failed to load with error: $errorCode")
-            }
-        }
-
-        moPubBanner.bannerAdListener = moPubViewbannerAdListener
-    }
-
-    override fun onDestroy() {
-        moPubBanner.destroy()
-
-        super.onDestroy()
-    }
-
 
 
 
